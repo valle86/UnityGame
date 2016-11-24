7fa9726063e932d079d7f47240bcbc441191d67b
valle86 <vlastimil.brodl@gmail.com>
2016-11-24 15:29:27 +0100

    Updated orbit Script
    Rudamentary orbit

--- Assets/Space/OrbitPlayer.cs
+++ Assets/Space/OrbitPlayer.cs
@@ -5,6 +5,8 @@ public class OrbitPlayer : MonoBehaviour {
 
     public Transform target;
     public float orbitDistance = 10.0f;
+
+    [SerializeField][Range(-180, 180)]
     public float orbitDegreesPerSec = 180.0f;
 
     // Use this for initialization
@@ -18,25 +20,18 @@ public class OrbitPlayer : MonoBehaviour {
         if (target != null)
         {
             // DISTANCE
-            // Keep us at orbitDistance from target
+            // Keep orbiter at orbitDistance from orbit target
             transform.position = target.position + (transform.position - target.position).normalized * orbitDistance;
-            transform.RotateAround(target.position, Vector3.up, orbitDegreesPerSec * Time.deltaTime);
+            transform.RotateAround(target.position, Vector3.up, -orbitDegreesPerSec * Time.deltaTime);
 
-
-            // DIRECTION TESTING AND VARIABLES. 
+            // ROTATION 
+            // based on vector between orbit-orbiter turned 90 degrees
+            // Vector between orbit - orbiter
             Vector3 dir = target.transform.position - transform.position;
+            // turn vector 90 degrees
             Vector3 ninetyDegrees = Vector3.Cross(Vector3.up, dir);
-            //Vector3 thirtyDegrees = Quaternion.Euler(0, 30, 0) * dir;
-            //dir = target.transform.InverseTransformDirection(dir);
-            //float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            Vector3 targetDir = target.transform.position - transform.position;
-            float angle = Vector3.Angle(ninetyDegrees, transform.forward);
-
-
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
-
-
-            //transform.rotation = Quaternion.AngleAxis(60, Vector3.up);
+            // Set Orbiter rotation
+            transform.rotation = Quaternion.LookRotation(ninetyDegrees);
 
         }
     }
@@ -44,17 +39,15 @@ public class OrbitPlayer : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-
         // Call from LateUpdate instead...
         // Orbit();
-
     }
+
     // Call from LateUpdate if you want to be sure your
     // target is done with it's move.
     void LateUpdate()
     {
-
         Orbit();
-
     }
+
 }
